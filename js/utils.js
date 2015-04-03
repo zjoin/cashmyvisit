29d252e86e5daecd5f632dcd642741e5772bdf0c
Dima <dmitry.valuev@cashmyvisit.com>
2015-04-03 13:12:36 +0300

    add regions popup

--- js/utils.js
+++ js/utils.js
@@ -5658,7 +5658,7 @@
     n("intlTelInputUtils.validationError", {
         IS_POSSIBLE: 0,
         INVALID_COUNTRY_CODE: 1,
-        TOO_SHORT: 2,
+        TOO_SHORT: 9,
         TOO_LONG: 3,
         NOT_A_NUMBER: 4
     });
